a7cd432a07dc5c005e4a7952b750761636ba304a
jovany <scotchnia@gmail.com>
2024-09-10 18:54:38 -0600

    Alpha

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({
+    dato1: '',
+    dato2: '',
+    dato3: '',
+    selectedOption: 'someOption',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form Data Submitted:', formData);
+
+    setFormData({
+      dato1: '',
+      dato2: '',
+      dato3: '',
+      selectedOption: 'someOption',
+    });
+
+    // Aqui va tu backend
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Super Forms">
+        <h1>Super Forms</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            name="dato1"
+            placeholder="Dato1"
+            value={formData.dato1}
+            onChange={handleInputChange}
+          />
+          <input
+            name="dato2"
+            placeholder="Dato2"
+            value={formData.dato2}
+            onChange={handleInputChange}
+          />
+          <input
+            name="dato3"
+            placeholder="Dato3"
+            value={formData.dato3}
+            onChange={handleInputChange}
+          />
+          <select
+            name="selectedOption"
+            value={formData.selectedOption}
+            onChange={handleInputChange}
+          >
+            <option value="someOption">Opcion1</option>
+            <option value="otherOption">Opcion2</option>
+          </select>
+          <button type="submit">Submit</button>
+        </form>
       </header>
     </div>
   );
 }
 
 export default App;
+
